096417375e2678c4cefdf58e66ccc250752cc9dd
Marcuspaulo18 <126567918+Marcuspaulo18@users.noreply.github.com>
2024-01-31 19:50:16 -0300

    Add files via upload

---
+++ conversor.py
@@ -0,0 +1,17 @@
+def me_pe(valor):
+    return (valor/3.281)
+
+def pe_me(valor):
+    return(valor*3.281)
+
+def kli(valor):
+    return (valor/2.2046)
+
+def lik(valor): 
+    return (valor*2.2046)
+
+def cel_fah(valor):
+    return (valor*1.8)+32
+
+def fah_cel(valor):
+   return (valor-32)/1.8
